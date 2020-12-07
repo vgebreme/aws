de2ab1ca77ccc65f41183831b359b65e68609ff3
vgebreme <75583919+vgebreme@users.noreply.github.com>
2020-12-06 21:15:28 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,10 @@
-var API_ENDPOINT_STR =  "https://7zm8p85chh.execute-api.us-east-1.amazonaws.com/Prod"
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_asCTSO7YW', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7ho7ab4k8ae3esfsh0v3h4ri5e', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://ah87n3rj31.execute-api.us-east-1.amazonaws.com/dev' ,
+    }
+};
